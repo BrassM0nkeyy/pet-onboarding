a7f2013cb15cefaf4a913d4916838ed946b642bf
Matt Grech <matthewgrech@leonss-mbp-2.war.can.ibm.com>
2018-09-24 10:07:55 -0400

    JS Chapter 2 lesson 2

---
+++ JS_tutorial/app.js
@@ -0,0 +1,9 @@
+//comments
+console.log("hello world");
+console.log(123123);
+var ing = "hi";
+console.log(ing);
+console.log(1,2,3,4);
+console.error("this is some error");
+console.clear();
+console.warn('this is a warning');
