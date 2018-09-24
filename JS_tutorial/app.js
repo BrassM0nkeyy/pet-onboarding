9a50fd53b32d3d4222a103cf192dad37df0b541d
Matt Grech <matthewgrech@leonss-mbp-2.war.can.ibm.com>
2018-09-24 15:56:23 -0400

    completed lesson 1-13 of chapter 2, with notes detailed each portion in notes.txt

--- JS_tutorial/app.js
+++
@@ -1,9 +0,0 @@
-//comments
-console.log("hello world");
-console.log(123123);
-var ing = "hi";
-console.log(ing);
-console.log(1,2,3,4);
-console.error("this is some error");
-console.clear();
-console.warn('this is a warning');
